53cd67b439a884fa7d91d8b51e75afe46108026f
liron <lironfinegold@gmail.com>
2016-10-04 15:16:20 +0300

    added player name and more use of "self"

--- humans.py
+++ humans.py
@@ -38,33 +38,32 @@ class Player(object):
     def draw_card(self, deck):
         return deck[0]
 
-    def play_card(self, card1, card2):
+    def play_card(self):
         is_valid = False
         while not is_valid:
-            print('Which card do you want to play? you have: %s and %s' % (card1, card2))
+            print('%s, Which card do you want to play? you have: %s and %s' % (self.name, self.card_num1, self.card_num2))
             player_move = int(input('Please pick one\n'))
-            is_valid = self.is_valid_move(player_move, card1, card2)
+            is_valid = self.is_valid_move(player_move)
         return player_move
 
-    def is_valid_move(self, player_move, card1, card2):
+    def is_valid_move(self, player_move):
         if player_move == 8:
             # TODO Once have names of cards change messages to include card names
             print("Oops! Can't discard the human card (8)! Try again...")
             return False
-        elif (card1 == 7 and card2 in (5, 6) and player_move == card2) or \
-             (card2 == 7 and card1 in (5, 6) and player_move == card1):
+        elif (self.card_num1 == 7 and self.card_num2 in (5, 6) and player_move == self.card_num2) or \
+             (self.card_num2 == 7 and self.card_num1 in (5, 6) and player_move == self.card_num1):
             # TODO Once have names of cards change messages to include card names
             print("Oops! Can't discard 5 or 6 when you have a 7!")
             return False
         else:
             return True
 
-    def update_hand(self, card1, card2, discarded):
-        if discarded[0] == card1:
-            self.card_num1 = card2
+    def update_hand(self):
+        if self.discarded[0] == self.card_num1:
+            self.card_num1 = self.card_num2
             self.card_num2 = None
         else:
-            self.card_num1 = card1
             self.card_num2 = None
 
 
@@ -79,26 +78,28 @@ class Player(object):
 # class Move(object):
 #     def __init__(self):
 
-deck1 = ['1', '1', '1', '1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '7', '8']
+deck1 = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]
 player1 = Player()
 player2 = Player()
 humans = Game(deck=deck1, players_list=[player1, player2])
 humans.prepare_deck(deck1)
 
-print (humans.deck)
+print(humans.deck)
 
 if len(humans.deck) == 15:
     for player in humans.players_list:
+        player.name = input('please enter your name\n')
         player.card_num1 = player.draw_card(humans.deck)
         humans.update_deck(humans.deck)
         print(humans.deck)
 while len(humans.deck) > 0:
     for player in humans.players_list:
-        player.card_num2=player.draw_card(humans.deck)
+        player.card_num2 = player.draw_card(humans.deck)
         humans.update_deck(humans.deck)
         # print(player.card_num1,player.card_num2)
-        player.discarded.insert(0, (player.play_card(player.card_num1, player.card_num2)))
-        player.update_hand(player.card_num1, player.card_num2, player.discarded)
+        player_move = player.play_card()
+        player.discarded.insert(0, player_move)
+        player.update_hand()
         print(humans.deck)
         if len(humans.deck) == 0:
             break
